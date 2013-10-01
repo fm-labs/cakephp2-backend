b2f41f3ec9c3fc2545db3848c7ea37cf2bc49ce7
flowmotion <beisskammer@gmail.com>
2013-10-01 11:10:48 +0200

    edited doc-block

--- Model/BackendUserRolesUsers.php
+++ Model/BackendUserRolesUsers.php
@@ -1,6 +1,11 @@
 <?php
 App::uses('BackendAppModel','Backend.Model');
 
+/**
+ * BackendUserRolesUsers - JoinModel
+ * 
+ * @todo add associations
+ */
 class BackendUserRolesUsers extends BackendAppModel {
-		
+	
 }
